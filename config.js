c6701e14bd82e70ad1dbed18f0c2f19b26afeeb3
pathwaysresearch <pathwaysresearch.in@gmail.com>
2026-04-19 19:01:14 +0530

    added readme

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Set this to your Cloud Run service URL after deployment
-window.BACKEND_URL = "https://YOUR_SERVICE_NAME.run.app";
+window.BACKEND_URL = "https://skillfit-score-713266569877.asia-south1.run.app";
